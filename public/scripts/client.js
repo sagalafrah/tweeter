a04c1dc6c76dbf33d254ea0f73ad2e316cca5a94
Sagal Afrah <afrahsagal@gmail.com>
2021-11-18 18:30:06 -0500

    Changed submit function to on

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,48 +10,78 @@ $(document).ready(function() {
   
 
 
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+// const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ]
 
 const renderTweets = function(tweets) {
+    
     for (const tweet of tweets) {
         const newTweet = createTweetElement(tweet)
-        $('#tweets-container').append(newTweet);
+        $('#tweets-container').prepend(newTweet);
       }
     };
   
-    $('.input form').submit(event => {
+    $('.input').on('submit', function(event) {
         event.preventDefault();
+        const tweetText = $(this).children('textarea').val();
+        console.log(tweetText);
+     
+        if (!tweetText) {
+          alert('Tweet is empty!');
+    
+        } else if (tweetText.length > 140) {
+          alert('Tweet is too long!');
+    
+        } else { 
+          
+          $.ajax('/tweets', {
+            data: $(this).serialize(),
+            method: 'POST'
+          });
+        }  
+
       })
 
-      $.ajax('/tweets', {
-        data: $(this).serialize(),
-        method: 'POST'
-      });
+    //   $.ajax('/tweets', {
+    //     data: $(this).serialize(),
+    //     method: 'POST'
+    //   });
     
 
+      const loadTweets = function () {
+        $.ajax('/tweets', {
+          method: 'GET',
+          dataType: 'JSON'
+        })
+          .then(tweets => renderTweets(tweets));
+      };
+    
+      loadTweets();
+
+      
+      
 const createTweetElement = function(tweet) {
     const exampleTweet = `<article class="tweet">
           <header>
