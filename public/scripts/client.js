df93d92ac065f946152a2e2da618951a64c6f5c6
Sagal Afrah <afrahsagal@gmail.com>
2021-11-18 19:29:21 -0500

    added Validation Errors, and allowed tweets to show on same page upon refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,36 +7,16 @@
 $(document).ready(function() {
 
 
-  
+
+    const escape =  function(str) {
+        let div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.exampleTweet;
+      }
 
 
-// const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
-
-const renderTweets = function(tweets) {
-    
+const renderTweets = function(tweets) {  
+    $('#tweets-container').empty();
     for (const tweet of tweets) {
         const newTweet = createTweetElement(tweet)
         $('#tweets-container').prepend(newTweet);
@@ -59,15 +39,14 @@ const renderTweets = function(tweets) {
           $.ajax('/tweets', {
             data: $(this).serialize(),
             method: 'POST'
+          })
+          .then(() => {
+            loadTweets();
+            $('#tweet-input').val('');
           });
         }  
 
       })
-
-    //   $.ajax('/tweets', {
-    //     data: $(this).serialize(),
-    //     method: 'POST'
-    //   });
     
 
       const loadTweets = function () {
@@ -86,10 +65,10 @@ const createTweetElement = function(tweet) {
     const exampleTweet = `<article class="tweet">
           <header>
             <img src= ${tweet.user.avatars}/>
-              <h4>${tweet.user.name}</h4>
-              <p>${tweet.user.handle}</p>
+              <h4>${escape(tweet.user.name)}</h4>
+              <p>${escape(tweet.user.handle)}</p>
           </header>
-          <p>${tweet.content.text}</p>
+          <p>${escape(tweet.content.text)}</p>
           <footer>
             <h5>${timeago.format(tweet.created_at)}</h5>
             <h5>flag retweet favorite</h5>
@@ -100,5 +79,5 @@ const createTweetElement = function(tweet) {
 };
 
 
-renderTweets(data);
+// renderTweets(data);
 });
